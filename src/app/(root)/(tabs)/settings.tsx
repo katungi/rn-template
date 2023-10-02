25ee7c4886eeef01cc01b8215185704dc4de36ce
Daniel Dennis <dankatdennis@gmail.com>
2023-10-02 11:37:53 +0300

    Initialize vercel-hosting

---
+++ src/app/(root)/(tabs)/settings.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Picker } from '@react-native-picker/picker';
+import { View } from '_context/Themed';
+import { MonoText as Text } from '_components/StyledText';
+
+//i18next
+import i18n from '_locales/index';
+
+//app version
+import appVersion from '_utils/version';
+
+export default function Settings() {
+  const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
+
+  const handleValueChange = (itemValue: string) => {
+    setSelectedLanguage(itemValue);
+    i18n.changeLanguage(itemValue);
+  };
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Picker
+        selectedValue={selectedLanguage}
+        onValueChange={handleValueChange}
+        style={{ height: 50, width: 150 }}
+      >
+        <Picker.Item label="English" value="en" />
+        <Picker.Item label="Spanish" value="es" />
+      </Picker>
+      <Text>Your app version is: {`${appVersion}`}</Text>
+    </View>
+  );
+}
